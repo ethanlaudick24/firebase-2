8a8916a4d4d38e6079fcca389142418922b16b47
Ethan <ethan.laudick24@d155.org>
2023-02-25 19:37:23 -0600

    Initial Commit

---
+++ firebase 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firebase 2
+//
+//  Created by Scott Laudick on 2/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ firebase 2UITests/firebase_2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  firebase_2UITestsLaunchTests.swift
+//  firebase 2UITests
+//
+//  Created by Scott Laudick on 2/25/23.
+//
+
+import XCTest
+
+final class firebase_2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
